bd31ab0b3be6d5372c1e2ea94575865d342c207a
AlanFernandes <josefernandes.alan@gmail.com>
2022-01-07 21:25:38 -0300

    Removendo função sem uso

--- js/components/menu.js
+++ js/components/menu.js
@@ -33,32 +33,20 @@ export const loadMenu = async (url) => {
         if (isSignedIn) {
           switch (id) {
             case 'menu-item-home':
-              //toggleRootChildrenDisplay(home.id);
               loadHome(url);
               break;
             case 'menu-item-list-data':
-              //toggleRootChildrenDisplay(list.id);
               loadList(url);
               break;
             case 'menu-item-template':
-              toggleRootChildrenDisplay(template.id);
+              
               break;
             case 'menu-item-how-to-use':
-              toggleRootChildrenDisplay(howToUse.id);
               loadHowToUse();
               break;
           }
         }
-      }
-
-      function toggleRootChildrenDisplay(id) {
-        const rootDiv = root.children;
-        new Array(...rootDiv).every((element) => {
-          if (element.id === id && !element.classList.contains('display-none')) return false;
-          element.id === id ? element.classList.remove('display-none') : element.classList.add('display-none');
-          return true;
-        });
-      }
+      }     
       // mediaQueries.topMenu();
     });
 
